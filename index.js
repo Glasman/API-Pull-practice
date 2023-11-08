e88374849376f080af18f309dbaa0ddd8948cd29
Mark Glasman <glasman.mark@gmail.com>
2023-11-08 15:35:31 -0500

    continued working on render function

--- index.js
+++ index.js
@@ -5,22 +5,20 @@ let state = {
 
 const getArtists = async () => {
   const response = await fetch('https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-fsa-et-web-ft-sf/artists')
-  const artists = await response.json()
-  console.log(artists)
-  state.allArtists = artists
-  render(artists.data)
+  const jsonResponse = await response.json()
+  state.allArtists = jsonResponse.data
+  console.log(jsonResponse)
+  render()
 }
-getArtists()
 
-function render(array) {
-  array.forEach((element) => {
-    console.log(element.name)
+function render() {
+  state.allArtists.forEach((element) => {
     const item = document.createElement('div');
     main.appendChild(item)
     item.innerText = 'test'
   }
   )
-
+  console.log(state.allArtists)
   
 }
-console.log(state.allArtists)
+getArtists()
